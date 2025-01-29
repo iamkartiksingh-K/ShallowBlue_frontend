a03e46ee5b1dbf6d5934e8a84c89c9c81561cfa2
iamkartiksingh-K <kartiksinghk2001@gmail.com>
2025-01-29 11:39:38 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Chessboard } from "react-chessboard";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="board">
+        <Chessboard id="BasicBoard" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
